6a7b6188b088029808072f7edfdd2b54c7a8ace3
Namdevpatil <dev321.net@gmail.com>
2024-01-03 23:01:12 +0530

    my first commit

---
+++ src/day4/Example1.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class Example1 {
+	
+	public static void main(String[] args) 
+	{
+		
+		int amount = 100000, time = 12;
+		
+		double roi = 2.5, simpleInterst = 0;
+				
+		simpleInterst = (amount*time*roi)/100;
+		
+		System.out.println("Simple Interst: "+simpleInterst);
+	}
+
+}
